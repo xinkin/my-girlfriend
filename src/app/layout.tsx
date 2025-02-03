442fcefb121319e2ced07c727f219c5e70c0cbd6
Yash <91452674+xinkin@users.noreply.github.com>
2025-02-03 23:44:19 +0530

    changed title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Babe, will you be my valentine?",
+  title: "Yash's Girlfriend?",
   description: "Valentine Proposal",
 };
 
