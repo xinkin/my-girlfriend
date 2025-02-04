da278a7b65ba9ae1994d32edfb7d40e46de01e12
Yash <91452674+xinkin@users.noreply.github.com>
2025-02-04 20:57:57 +0530

    hidden path

---
+++ src/app/kinks/page.tsx
@@ -0,0 +1,11 @@
+const Kinks = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">
+        she can&apos;t control her moans when I fuck her ;)
+      </h1>
+    </div>
+  );
+};
+
+export default Kinks;
