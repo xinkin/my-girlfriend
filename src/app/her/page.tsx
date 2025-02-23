a43513d6f56642461b2f013324362d629f83a4ad
Yash <91452674+xinkin@users.noreply.github.com>
2025-02-23 20:37:14 +0530

    added girlfie insta

---
+++ src/app/her/page.tsx
@@ -0,0 +1,13 @@
+const Her = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <a href="https://www.instagram.com/riddhimaaakapoor/" target="_blank">
+        <button className="text-4xl bg-pink-300 hover:text-5xl hover:p-5 text-neutral-700 font-bold p-4 rounded-3xl">
+          girlfies insta
+        </button>
+      </a>
+    </div>
+  );
+};
+
+export default Her;
